6921f9c4b1cad21e59c3e8829691b56692ffbca4
Marcel75Git <55857370+Marcel75Git@users.noreply.github.com>
2020-01-28 13:55:57 +0100

    añadidas de la excepcion:saleNotInitiatedeXCEPTION

---
+++ src/Excepciones/SaleNotInitiatedException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Excepciones;
+
+/**
+ *
+ * @author mfoulouyvesmarcel
+ */
+public class SaleNotInitiatedException  extends Exception{
+    public SaleNotInitiatedException(String message){
+        super(message);
+    }
+}
